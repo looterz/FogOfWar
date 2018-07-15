ca55cf6b8089955ff38ed6aae5b999a809266f46
Lars Jørgen Solberg <supersolberg@gmail.com>
2018-07-15 12:02:43 +0200

    initial commit

---
+++ Source/FogOfWar/Public/FogOfWarWorker.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "HAL/Runnable.h"
+/**
+* Worker thread for updating the fog of war data.
+*/
+class AFogOfWarManager;
+
+class AFogOfWarWorker : public FRunnable
+{
+	//Thread to run the FRunnable on
+	FRunnableThread* Thread;
+
+	//Pointer to our manager
+	AFogOfWarManager* Manager;
+
+	//Thread safe counter
+	FThreadSafeCounter StopTaskCounter;
+
+public:
+	AFogOfWarWorker();
+	AFogOfWarWorker(AFogOfWarManager* manager);
+	virtual ~AFogOfWarWorker();
+
+	//FRunnable interface
+	virtual bool Init();
+	virtual uint32 Run();
+	virtual void Stop();
+
+	//Method to perform work
+	void UpdateFowTexture();
+
+	bool bShouldUpdate = false;
+
+	void ShutDown();
+};
